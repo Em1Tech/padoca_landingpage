596fae7efb3f7daf72898f4b92de50000fcf4568
Caio Emidio <caio.emidio@gmail.com>
2024-01-30 00:53:35 +0000

    update

--- components/sections/main.vue
+++ components/sections/main.vue
@@ -6,9 +6,9 @@
         <p class="uppercase text-xl md:text-2xl">{{ $t("HOW_ABOUT_TRYING_SOMETHING_NEW_TODAY") }}</p>
       </div>
 
-      <div class="flex flex-col md:flex-row justify-around items-center gap-2 my-4 md:my-2">
-        <div class="w-[330px] md:w-[330px] cursor-pointer" v-for="option in options" :key="option" @click="resolveLink(option.link)">
-          <img :src="option.image" alt="" />
+      <div class="flex flex-col md:flex-row justify-around items-center gap-2 my-4 md:my-2 mx-4">
+        <div class="w-full md:w-[330px]  cursor-pointer" v-for="option in options" :key="option" @click="resolveLink(option.link)">
+          <img  class="h-64 w-full object-center object-cover" :src="option.image" alt="" />
           <button
             class="bg-[#ffc301] uppercase text-[#2E2E2E] w-full py-4 font-bold text-2xl flex flex-col items-center"
           >
@@ -33,7 +33,7 @@ export default defineComponent({
           comments: "PICKS_DELIVERY"
         },
         {
-          image: "/img/party.png",
+          image: "/img/party.jpg",
           title: "ASK_FOR_PARTY",
           link: "https://www.checktime.ie/en/store/padoca/1489057",
           comments: "CAKES_KITS"
